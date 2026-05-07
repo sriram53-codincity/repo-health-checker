57b6875585041288d18a05271de3548b5e257ec4
sriram53-codincity <sriram.s@codincity.com>
2026-05-07 13:19:23 +0530

    check.sh will show errors with count

--- check.sh
+++ check.sh
@@ -7,7 +7,7 @@ error_found=0
 # Check 1: README exists
 if [ ! -f README.md ]; then
   echo "ERROR: README.md file missing"
-  error_found=1
+  ((error_found++))
 fi
 
 # Check 2: README has more than 5 lines
@@ -16,20 +16,20 @@ if [ -f README.md ]; then
 
   if [ "$lines" -lt 5 ]; then
     echo "ERROR: README.md has less than 5 lines"
-    error_found=1
+    ((error_found++))
   fi
 fi
 
 # Check 3: .gitignore exists
 if [ ! -f .gitignore ]; then
   echo "ERROR: .gitignore file missing"
-  error_found=1
+  ((error_found++))
 fi
 
 # Check 4: No .env files
 if find . -name ".env" | grep -q .; then
   echo "ERROR: .env file detected"
-  error_found=1
+  ((error_found++))
 fi
 
 # Check 5: Commit message has at least 5 words
@@ -38,12 +38,12 @@ word_count=$(echo "$commit_msg" | wc -w)
 
 if [ "$word_count" -lt 5 ]; then
   echo "ERROR: Commit message must contain at least 5 words"
-  error_found=1
+  ((error_found++))
 fi
 
 # Final Result
 if [ "$error_found" -eq 1 ]; then
-  echo "One or more checks failed."
+  echo "$error_found"
   exit 1
 else
   echo "All checks passed!"
